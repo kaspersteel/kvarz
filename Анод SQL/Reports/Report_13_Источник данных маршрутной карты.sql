523cb939c1c7835659207a8533f782e2ebb289c7
kaspersteel <kasper.steel@gmail.com>
2024-08-02 15:52:09 +0300

    ok

--- Анод SQL/Reports/Report_13_Источник данных маршрутной карты.sql
+++ Анод SQL/Reports/Report_13_Источник данных маршрутной карты.sql
@@ -9,7 +9,7 @@ SELECT o.* ,
        sotrud5.attr_1894_ sotr5 ,
        materials.attr_401_ name_materials ,
        concat(sprav_matterials.attr_401_, sprav_matterials1.attr_401_) zagotovka ,
-       round(o.attr_1908_, 3) massa ,
+       round(o.attr_3271_, 3) massa ,
        CASE
            WHEN o.attr_2882_ IS TRUE THEN 'КАРТА МАРШРУТНОГО ТЕХНОЛОГИЧЕСКОГО ПРОЦЕССА МЕХАНИЧЕСКОЙ ОБРАБОТКИ ТИПОВАЯ'
            ELSE 'КАРТА МАРШРУТНОГО ТЕХНОЛОГИЧЕСКОГО ПРОЦЕССА МЕХАНИЧЕСКОЙ ОБРАБОТКИ'
@@ -17,9 +17,9 @@ SELECT o.* ,
 FROM registry.object_519_ o
 LEFT JOIN registry.object_400_ materials ON materials.id = o.attr_2381_
 LEFT JOIN registry.object_400_ sprav_matterials ON sprav_matterials.id = o.attr_1876_
-AND sprav_matterials.is_deleted <> TRUE
+AND sprav_matterials.is_deleted IS FALSE
 LEFT JOIN registry.object_400_ sprav_matterials1 ON sprav_matterials1.id = o.attr_1877_
-AND sprav_matterials1.is_deleted <> TRUE
+AND sprav_matterials1.is_deleted IS FALSE
 LEFT JOIN registry.object_441_ p ON p.id = o.attr_522_
 LEFT JOIN registry.object_301_ det ON det.id = o.attr_520_
 LEFT JOIN registry.object_17_ sotrud ON sotrud.id = o.attr_2047_
@@ -27,6 +27,6 @@ LEFT JOIN registry.object_17_ sotrud1 ON sotrud1.id = o.attr_2048_
 LEFT JOIN registry.object_17_ sotrud2 ON sotrud2.id = o.attr_2049_
 LEFT JOIN registry.object_17_ sotrud4 ON sotrud4.id = o.attr_2050_
 LEFT JOIN registry.object_17_ sotrud5 ON sotrud5.id = o.attr_2051_
-WHERE o.is_deleted <> TRUE
+WHERE o.is_deleted IS FALSE
   AND o.attr_2045_ = 1
   AND o.id = '{superId}';
