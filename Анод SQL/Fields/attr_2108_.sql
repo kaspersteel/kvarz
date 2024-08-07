9f94b90b479470d5d22855229bb994cfd2a72252
kaspersteel <kasper.steel@gmail.com>
2024-08-07 16:48:49 +0300

    ок

--- Анод SQL/Fields/attr_2108_.sql
+++ Анод SQL/Fields/attr_2108_.sql
@@ -1,13 +1,13 @@
-CASE WHEN o.attr_2626_ is NOT NULL THEN o.attr_2626_ ELSE
+CASE WHEN o.attr_2108_ is NOT NULL THEN o.attr_2108_ ELSE
 CASE
           WHEN tech_card_prod.attr_2554_ IS TRUE THEN (
-          tech_card_prod.attr_1879_ * CEILING (
+          tech_card_prod.attr_1908_ * CEILING (
           comp_order.attr_1896_::NUMERIC(5, 2) / tech_card_prod.attr_2907_::NUMERIC(5, 2)
           )
           ) + (
-          COALESCE(tech_card_prod.attr_2884_, 0) * CEILING (
+          COALESCE(tech_card_prod.attr_4105_, 0) * CEILING (
           comp_order.attr_1896_::NUMERIC(5, 2) / tech_card_prod.attr_2555_::NUMERIC(5, 2)
           )
           )
           WHEN tech_card_prod.attr_2554_ IS FALSE THEN comp_order.attr_1896_ * tech_card_prod.attr_1908_
-END END
+END
--- Анод SQL/Fields/attr_4105_.sql
+++ Анод SQL/Fields/attr_4105_.sql
@@ -1,43 +1,35 @@
-   SELECT -->>
-          CASE
-                    WHEN o.attr_3227_ IS TRUE THEN o.attr_1908_
-                    ELSE CASE mat_for_mk.attr_1373_
+CASE mat_for_mk.attr_1373_
                               WHEN 1 THEN (
-                              PI() * (o.attr_1873_ / 2000) ^ 2 * o.attr_1879_ / 1000
+                              PI() * (o.attr_1873_ / 2000) ^ 2 * o.attr_2884_ / 1000
                               ) * dens_for_mk.attr_1359_
                               WHEN 2 THEN (
-                              o.attr_1875_ / 1000 * o.attr_1879_ / 1000 * o.attr_1874_ / 1000
+                              o.attr_1875_ / 1000 * o.attr_2884_ / 1000 * o.attr_1874_ / 1000
                               ) * dens_for_mk.attr_1359_
                               WHEN 3 THEN (
                               o.attr_1874_ / 1000 * (
                               o.attr_1875_ / 1000 + o.attr_3270_ / 1000 - o.attr_1874_ / 1000
-                              ) * o.attr_1879_ / 1000
+                              ) * o.attr_2884_ / 1000
                               ) * dens_for_mk.attr_1359_
                               WHEN 4 THEN (
-                              2 * SQRT(3) * (o.attr_3270_ / 1000 / 2) ^ 2 * o.attr_1879_ / 1000
+                              2 * SQRT(3) * (o.attr_3270_ / 1000 / 2) ^ 2 * o.attr_2884_ / 1000
                               ) * dens_for_mk.attr_1359_
                               WHEN 5 THEN (
                               PI() * (
                               (o.attr_1873_ / 1000 / 2) ^ 2 - (
                               COALESCE(o.attr_3220_, (o.attr_1873_ - o.attr_1874_)) / 1000 / 2
                               ) ^ 2
-                              ) * (o.attr_1879_ / 1000)
+                              ) * (o.attr_2884_ / 1000)
                               ) / 4 * dens_for_mk.attr_1359_
                               WHEN 7 THEN (
-                              4 * o.attr_1874_ / 1000 * ((o.attr_1875_ / 1000) - (o.attr_1874_ / 1000)) * (o.attr_1879_ / 1000)
+                              4 * o.attr_1874_ / 1000 * ((o.attr_1875_ / 1000) - (o.attr_1874_ / 1000)) * (o.attr_2884_ / 1000)
                               ) * dens_for_mk.attr_1359_
                               WHEN 8 THEN (
                               2 * (o.attr_1874_ / 1000) * (
                               o.attr_1875_ / 1000 + o.attr_3270_ / 1000 -2 * o.attr_1874_ / 1000
-                              ) * (o.attr_1879_ / 1000)
+                              ) * (o.attr_2884_ / 1000)
                               ) * dens_for_mk.attr_1359_
                               WHEN 9 THEN (
-                              (o.attr_3270_ / 1000) * (o.attr_1874_ / 1000) -2 * (o.attr_1874_ / 1000) * (o.attr_1875_ / 1000 - o.attr_1874_ / 1000) * (o.attr_1879_ / 1000)
+                              (o.attr_3270_ / 1000) * (o.attr_1874_ / 1000) -2 * (o.attr_1874_ / 1000) * (o.attr_1875_ / 1000 - o.attr_1874_ / 1000) * (o.attr_2884_ / 1000)
                               ) * dens_for_mk.attr_1359_
                               ELSE NULL
                     END
-          END
-     FROM registry.object_519_ o -->>
-    WHERE o.is_deleted IS FALSE -->>
- GROUP BY -->>
- TODO::дописать формулу для групповой заготовки
