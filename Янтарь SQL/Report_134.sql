dcd5e41152e0bb9bf43b9e204501e9968d6132ca
kaspersteel <kasper.steel@gmail.com>
2024-12-10 16:44:16 +0300

    draft

--- Янтарь SQL/Report_134.sql
+++ Янтарь SQL/Report_134.sql
@@ -151,3 +151,108 @@
           ) AS time_proc
      FROM base
     WHERE base.array_time IS NOT NULL
+--------------------------------------------------//----------------------------------------------------------
+    WITH base AS (
+	SELECT
+		fio_pac.attr_1985_ AS fio,
+		o.attr_4005_ AS hist_id,
+		nom_cat.attr_444_ AS nom,
+		o.attr_4032_ AS date_rasp,
+		plan.attr_784_ AS id_proc,
+		proc.attr_695_ AS name_proc,
+		P.*,
+		(
+		SELECT ARRAY
+			[ ARRAY_AGG ( t_ranges.START ), ARRAY_AGG ( t_ranges.finish ) ] 
+		FROM
+			(
+			SELECT
+				timeslots.attr_4754_ AS START,
+				timeslots.attr_4753_ AS finish,
+				T.ID AS ID 
+			FROM
+				UNNEST ( P.array_time ) AS T ( ID )
+				LEFT JOIN registry.object_4002_ timeslots ON timeslots.ID = T.ID 
+			) AS t_ranges 
+		) AS array_ranges 
+	FROM
+		registry.object_4000_ o /*строится таблица соответствия id процедуры в плане и её времени по строке расписания*/
+		LEFT JOIN LATERAL (
+		VALUES
+			( o.attr_4008_, o.attr_4007_ ),
+			( o.attr_4010_, o.attr_4009_ ),
+			( o.attr_4012_, o.attr_4011_ ),
+			( o.attr_4014_, o.attr_4013_ ),
+			( o.attr_4016_, o.attr_4015_ ),
+			( o.attr_4018_, o.attr_4017_ ),
+			( o.attr_4020_, o.attr_4019_ ),
+			( o.attr_4022_, o.attr_4021_ ),
+			( o.attr_4024_, o.attr_4023_ ),
+			( o.attr_4026_, o.attr_4025_ ),
+			( o.attr_4028_, o.attr_4027_ ),
+			( o.attr_4030_, o.attr_4029_ ),
+			( o.attr_4163_, o.attr_4162_ ),
+			( o.attr_4166_, o.attr_4165_ ),
+			( o.attr_4169_, o.attr_4168_ ),
+			( o.attr_4252_, o.attr_4251_ ),
+			( o.attr_4444_, o.attr_4443_ ),
+			( o.attr_4447_, o.attr_4446_ ),
+			( o.attr_4456_, o.attr_4455_ ),
+			( o.attr_4459_, o.attr_4458_ ) 
+		) AS P ( "proc_in_plan", "array_time" ) ON P.array_time IS NOT NULL 
+		AND P.array_time != ARRAY [ 1 ]
+		LEFT JOIN registry.object_783_ plan ON plan.ID = P.proc_in_plan 
+		AND NOT plan.is_deleted
+		LEFT JOIN registry.object_694_ proc ON plan.attr_784_ = proc.ID 
+		AND NOT proc.is_deleted
+		LEFT JOIN registry.object_45_ fio_pac ON o.attr_4004_ = fio_pac.ID 
+		AND NOT fio_pac.is_deleted
+		LEFT JOIN registry.object_127_ nom_cat ON o.attr_4006_ = nom_cat.ID 
+		AND NOT nom_cat.is_deleted 
+	WHERE
+		NOT o.is_deleted 
+		AND o.attr_4005_ = 2563 
+	ORDER BY
+		4 DESC,
+		1,
+		8 
+	) SELECT
+	base.*,
+	(
+	SELECT
+		string_agg ( period, '\n' ) 
+	FROM
+		(
+		SELECT UNNEST
+			( array_remove ( ARRAY_AGG ( x.c1 ), NULL ) ) || '-' || UNNEST ( array_remove ( ARRAY_AGG ( x.c2 ), NULL ) ) AS period 
+		FROM
+			(
+			SELECT
+			CASE
+					
+				WHEN
+					timeslots.START NOT IN (
+					SELECT
+						timeslots.finish 
+					FROM
+						UNNEST ( base.array_ranges [1], base.array_ranges [2] ) AS timeslots ( START, finish )) THEN
+							timeslots.START 
+						END AS c1,
+					CASE
+							
+							WHEN timeslots.finish NOT IN (
+							SELECT
+								timeslots.START 
+							FROM
+								UNNEST ( base.array_ranges [1], base.array_ranges [2] ) AS timeslots ( START, finish )) THEN
+									timeslots.finish 
+								END AS c2 
+							FROM
+								UNNEST ( base.array_ranges [1], base.array_ranges [2] ) AS timeslots ( START, finish ) 
+							) x 
+						) y 
+					) 
+				FROM
+					base 
+			WHERE
+	base.array_time IS NOT NULL
